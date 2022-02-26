fbd50e397fc899d8cc87ac656897c9de2f718e02
Marcos Ferole <marcaoferole@pop-os.localdomain>
2022-02-26 17:38:08 -0400

    forma-dinamica-headtitle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import styles from '../styles/home.module.scss';
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      
       <h1 className={styles.title}>
         Hello <span>World</span>
       </h1>
-    </div>
+    </>
   )
 }

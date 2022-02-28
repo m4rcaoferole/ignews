653fd4763d19529de939cc287b2f25423f06eeca
Marcos Ferole <marcaoferole@pop-os.localdomain>
2022-02-28 18:06:17 -0400

    autentication-nextauth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,25 +1,27 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 import styles from './styles.module.scss';
 
 export const SignInButton = () => {
-  const isUserLoggedIn = true;
-
-  return isUserLoggedIn ? (
+  const { data: session } = useSession()
+  
+  return session ? (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="var(--green-300)"/>
-      Marcos Ferole
-      
+      {session.user.name}      
       <FiX color="var(--gray-300)" className={styles.closeIcon} />
     </button>
   ) : (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn()}
     >
       <FaGithub color="var(--yellow-500)"/>
       Sign in with Github

7f7eb54e89087b7756adc7e84b3f10b66f0bd58b
Marcos Ferole <marcaoferole@pop-os.localdomain>
2022-02-27 20:36:30 -0400

    static-side-generation-cliente-side-ssr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribreButton'
@@ -28,7 +28,7 @@ export default function Home({ product }: HomeProps) {
             <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton priceId={product.priceId}/>
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
 
@@ -39,7 +39,7 @@ export default function Home({ product }: HomeProps) {
 }
 
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KXryxALYzUR1JyTzviyKECe')
 
   const product = {
@@ -53,8 +53,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24hrs
   }
 }
-
-

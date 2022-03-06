513cddc4f214d6ff37ba1f2017a1aa28249436a6
Marcos Ferole <marcaoferole@pop-os.localdomain>
2022-03-05 20:11:48 -0400

    customer-subscription-update-deleted-no-stripe-faunadb

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -20,7 +20,11 @@ export const config = {
   },
 };
 
-const relevantEvent = new Set(["checkout.session.completed"]);
+const relevantEvent = new Set([
+  "checkout.session.completed",
+  "customer.subscription.updated",
+  "customer.subscription.deleted",
+]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
@@ -44,13 +48,25 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (relevantEvent.has(type)) {
       try {
         switch (type) {
+          case "customer.subscription.updated":
+          case "customer.subscription.deleted":
+            const subscription = event.data.object as Stripe.Subscription
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            )
+
+            break;
           case "checkout.session.completed":
             const checkoutSession = event.data
               .object as Stripe.Checkout.Session;
 
             await saveSubscription(
               checkoutSession.subscription.toString(),
-              checkoutSession.customer.toString()
+              checkoutSession.customer.toString(),
+              true
             );
 
             break;

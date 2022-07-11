368f9134c6bd1c0e90b95517a3d13e34c1d57500
Marcos Vinicius <victor_jv380@hotmiail.com>
2022-07-10 22:49:08 -0400

    feat: add pag post for post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,9 +1,9 @@
 import { GetStaticProps } from "next";
 import Head from "next/head";
 import Prismic from "@prismicio/client";
-
 import { getPrismicClient } from "../../services/prismic";
 import { RichText } from "prismic-dom";
+import Link from "next/link"
 
 import styles from "./styles.module.scss";
 
@@ -27,11 +27,14 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map((post) => (
-            <a key={post.slug} href="#">
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            // eslint-disable-next-line react/jsx-key
+            <Link href={`/posts/${post.slug}`}>
+              <a key={post.slug}>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>

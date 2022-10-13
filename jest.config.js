83709f962a6292df9b6572a45ef4bdeb879f31d6
Marcos Vinicius <marcao.adsistema@gmail.com>
2022-10-12 20:17:37 -0400

    feat: testing header

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,8 @@ module.exports = {
   setupFilesAfterEnv: [
     "<rootDir>/src/tests/setupTests.ts"
   ],
-  // transform: {
-  //   "^.+\\(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
-  // },
-  // testEnvironment: 'jsdom'
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+  },
   testEnvironment: 'jest-environment-jsdom' 
 }

c225ac93810556793fbe080bb4d2466c9ebfc8d7
Marcos Vinicius <marcao.adsistema@gmail.com>
2022-10-13 21:30:40 -0400

    fix: lib ts-jest decreciada

--- src/components/SignInButton/SignInButton.spec.tsx
+++ src/components/SignInButton/SignInButton.spec.tsx
@@ -1,19 +1,23 @@
 import { render, screen } from "@testing-library/react"
+import { mocked } from 'jest-mock'
+import { useSession } from 'next-auth/react'
 import { SignInButton } from "."
 
-jest.mock('next-auth/react', () => {
-  return {
-    useSession() {
-      return [null, false]
-    }
-  }
-})
+jest.mock('next-auth/react')
 
 describe('SignInButton component', () => {
   it('renders correctly when user is not authenticated', () => {
-    render(
-      <SignInButton />
-    )
+    const useSessionMocked = mocked(useSession)
+
+    useSessionMocked.mockReturnValue([null, false])
+
+    render(<SignInButton />)
+  
+    expect(screen.getByText('Sign in with Github')).toBeInTheDocument();
+  })
+
+  it('renders correctly when user is authenticated', () => {
+    render(<SignInButton />)
   
     expect(screen.getByText('Sign in with Github')).toBeInTheDocument();
   })
